01692bc67b87ad402072fbb63a0a4f27d96368f6
WelbennysKennedy <welbeneskenedy@gmail.com>
2026-03-05 23:20:54 +0000

    configurações express

--- src/serve.ts
+++ src/serve.ts
@@ -1,1 +1,13 @@
-const teste = 'teste'
+import express from 'express';
+
+const port = 3000;
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Home page');
+});
+
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
