6fb765f51c81755616f734915466ad675e4cd6ae
WelbennysKennedy <welbeneskenedy@gmail.com>
2026-03-09 23:03:05 +0000

    adicionado rotas de listagens dos filmes

--- src/serve.ts
+++ src/serve.ts
@@ -1,10 +1,23 @@
+import 'dotenv/config';
 import express from 'express';
+import { PrismaClient } from '@prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
 
 const port = 3000;
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Home page');
+const connectionString = process.env.DATABASE_URL;
+
+if (!connectionString) {
+  throw new Error('DATABASE_URL is not defined in .env');
+}
+
+const adapter = new PrismaPg({ connectionString });
+const prisma = new PrismaClient({ adapter });
+
+app.get('/movies', async (req, res) => {
+  const movies = await prisma.movies.findMany();
+  res.json(movies);
 });
 
 
