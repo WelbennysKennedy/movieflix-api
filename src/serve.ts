ed3524463c5ad2f6ded190d345af057d86cf45f3
WelbennysKennedy <welbeneskenedy@gmail.com>
2026-03-14 23:51:00 +0000

    adicionado correção de filmes duplicados no banco de dados

--- src/serve.ts
+++ src/serve.ts
@@ -1,56 +1,69 @@
-import 'dotenv/config';
-import express from 'express';
-import { PrismaClient } from '@prisma/client';
-import { PrismaPg } from '@prisma/adapter-pg';
+import 'dotenv/config'
+import express from 'express'
+import { PrismaClient } from '@prisma/client'
+import { PrismaPg } from '@prisma/adapter-pg'
 
-const port = 3000;
-const app = express();
+const port = 3000
+const app = express()
 
-app.use(express.json());
+app.use(express.json())
 
-const connectionString = process.env.DATABASE_URL;
+const connectionString = process.env.DATABASE_URL
 
 if (!connectionString) {
-  throw new Error('DATABASE_URL is not defined in .env');
+    throw new Error('DATABASE_URL is not defined in .env')
 }
 
-const adapter = new PrismaPg({ connectionString });
-const prisma = new PrismaClient({ adapter });
+const adapter = new PrismaPg({ connectionString })
+const prisma = new PrismaClient({ adapter })
 
 app.get('/movies', async (_, res) => {
-  const movies = await prisma.movies.findMany({
-    orderBy: {
-      title: 'asc'
-    },
-    include: {
-      genres: true,
-      languages: true,
-    }
-  });
-  res.json(movies);
-});
+    const movies = await prisma.movies.findMany({
+        orderBy: {
+            title: 'asc',
+        },
+        include: {
+            genres: true,
+            languages: true,
+        },
+    })
+    res.json(movies)
+})
 
 app.post('/movies', async (req, res) => {
+    const { title, genre_id, language_id, oscar_count, release_date } = req.body
 
- const { title, genre_id, language_id, oscar_count, release_date } = req.body;
-  
-try {
-  await prisma.movies.create({
-      data: {
-      title,
-      genre_id,
-      language_id,
-      oscar_count,
-      release_date: new Date(release_date)
+    try {
+        // veririficar no banco de dados se ja existe um filme com o nome igual ao que esta sendo enviado.
+
+        const movieWhithSameTitle = await prisma.movies.findFirst({
+            where: { title: { equals: title, mode: 'insensitive' } },
+        })
+
+        if (movieWhithSameTitle) {
+            return res.status(409).json({
+                error: 'A movie with the same title already exists.',
+            })
+        }
+
+        await prisma.movies.create({
+            data: {
+                title,
+                genre_id,
+                language_id,
+                oscar_count,
+                release_date: new Date(release_date),
+            },
+        })
+
+        res.status(201).send()
+    } catch (error) {
+        res.status(500).json({
+            error: 'An error occurred while creating the movie.',
+        })
     }
-  });
-
-  res.status(201).send();
-} catch (error) {
-  res.status(500).json({ error: 'An error occurred while creating the movie.' });
-}
-});
+})
 
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
+    console.log(`Server is running on http://localhost:${port}`)
+})
